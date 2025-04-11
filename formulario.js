d336146077e486c34d868bea9cb209baa26c27e7
GustavoNeneve <47082428+GustavoNeneve@users.noreply.github.com>
2025-04-10 23:06:10 -0300

    Refactor phone number formatting and update form instructions for clarity

--- formulario.js
+++ formulario.js
@@ -50,13 +50,13 @@ document.addEventListener("DOMContentLoaded", function () {
       if (telefone.length > 11) telefone = telefone.slice(0, 11);
 
       if (telefone.length > 10) {
-        telefone = telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3");
+      telefone = telefone.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})$/, "($1) $2 $3-$4");
       } else if (telefone.length > 6) {
-        telefone = telefone.replace(/^(\d{2})(\d{4})(\d{0,4})$/, "($1) $2-$3");
+      telefone = telefone.replace(/^(\d{2})(\d{4})(\d{0,4})$/, "($1) $2-$3");
       } else if (telefone.length > 2) {
-        telefone = telefone.replace(/^(\d{2})(\d{0,4})$/, "($1) $2");
+      telefone = telefone.replace(/^(\d{2})(\d{0,4})$/, "($1) $2");
       } else {
-        telefone = telefone.replace(/^(\d*)$/, "($1");
+      telefone = telefone.replace(/^(\d*)$/, "($1");
       }
 
       event.target.value = telefone;
